6377405fed73ffec1f563f369d6e940244aaff13
Ali Syaifudin <muhammad.ali.syaifudin@hotmail.com>
2023-02-19 15:19:18 +0700

    add table of details

--- utils/safe_mkdir.py
+++ utils/safe_mkdir.py
@@ -1,6 +1,12 @@
 from os import mkdir
 
 def safe_mkdir(path: str):
+    """
+    Create a directory if it doesn't exist
+
+    Args:
+        path (str): Path to the directory to create
+    """
     try:
         mkdir(path)
         print(f"Creating {path} dir in Data dir")
