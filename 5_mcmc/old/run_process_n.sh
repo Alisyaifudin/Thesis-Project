7174900223cde1c4f9c5f6047b14b143aae2d170
Ali Syaifudin <muhammad.ali.syaifudin@hotmail.com>
2023-08-03 00:06:36 +0700

    restructure

--- 5_mcmc/old/run_process_n.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for ((i=0; i<6; i++))
-do
-    current_time=$(date +"%Y-%m-%dT%H:%M:%S")
-    echo "Running n $i at $current_time"
-    python program-n.py -d $i all
-    current_time=$(date +"%Y-%m-%dT%H:%M:%S")
-    echo "Finished n $i at $current_time"
-done
